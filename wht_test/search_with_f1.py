cb040bcef8165d6bfb824352e94b5b0fa1b5af2d
wuhongtao <wuhongtao@kuaishou.com>
2024-09-11 14:15:41 +0800

    fix

--- wht_test/search_with_f1.py
+++ wht_test/search_with_f1.py
@@ -1,7 +1,10 @@
-import paradance as para
 import pandas as pd
 import time
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.dirname('/home/wuhongtao/ParaDance/src/paradance')))
+import paradance as para
+
 
 
 def process_data(data_path, column_names, feature_cols, label_col, save_path, product_name=None):
